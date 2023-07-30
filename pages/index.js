61a7ba1bc2ba673b2591660c0af3d328da23a84c
Euseung <yousi7@naver.com>
2023-07-30 23:47:24 +0900

    main

--- pages/index.js
+++ pages/index.js
@@ -4,51 +4,41 @@ import Introduction from "../components/home/introduction";
 import Animation from "../components/home/animation";
 import { useEffect } from "react";
 import Contact from "../components/contact/contact";
+import Projects from "../components/projects/projects";
+import { NOTION_DATABASE_ID, NOTION_TOKEN } from "../config";
 
-export default function Home() {
-  // function test() {
-  //   const horizontalUnderLine = document.getElementById("horizontal-underline");
-  //   const horizontalMenus = document.querySelectorAll(".menue");
-  //   console.log("horizontalUnderLine", horizontalMenus);
-
-  //   function horizontalIndicator(e) {
-  //     horizontalUnderLine.style.left = e.currentTarget.offsetLeft + "px";
-  //     horizontalUnderLine.style.width = e.currentTarget.offsetWidth + "px";
-  //     horizontalUnderLine.style.top =
-  //       e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
-  //   }
-  //   horizontalMenus.forEach((menu) =>
-  //     menu.addEventListener("cursor", (e) => horizontalIndicator(e))
-  //   );
-  // }
-  // useEffect(() => {
-  //   test();
-  // }, []);
-
-  // const section = document.querySelectorAll("section");
-  // const contents = document.querySelectorAll(".content");
-
-  // const firstTop = contents[0].offsetTop;
-  // const secondTop = contents[1].offsetTop;
-
-  // section[0].onclick = function () {
-  //   window.scroll({ top: firstTop, behavior: "smooth" });
-  // };
-  // section[1].onclick = function () {
-  //   window.scroll({ top: secondTop, behavior: "smooth" });
-  // };
-
-  // {
-  //   console.log("aaaaa : ", section);
-  // }
-
+export default function Home({ projectData }) {
   return (
     <>
-      <div className="container mx-auto">
+      <div className="container mx-auto ">
         <Animation />
         <Introduction />
+        <Projects projectData={projectData} />
         <Contact />
       </div>
     </>
   );
+}
+
+//  빌드시 호출
+export async function getStaticProps() {
+  //  Notion API (Query a database)
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+      Authorization: `Bearer ${NOTION_TOKEN}`,
+    },
+  };
+
+  const reponse = await fetch(
+    `https://api.notion.com/v1/databases/${NOTION_DATABASE_ID}/query`,
+    options
+  );
+
+  const projectData = await reponse.json();
+
+  return { props: { projectData }, revalidate: 60 };
 }
